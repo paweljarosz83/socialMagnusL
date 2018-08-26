16d43e3a72e9d8cf20e79cb57b82064281694cb2
paweljarosz83 <pawel.jarosz@tfkable.pl>
2018-08-26 15:42:53 +0200

    1st

---
+++ src/main/java/com/paweljarosz/twpro/TwitterPromoApplication.java
@@ -0,0 +1,12 @@
+package com.paweljarosz.twpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterPromoApplication.class, args);
+	}
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,65 @@
+
+create table UserConnection (
+  userId varchar(255) not null,
+  providerId varchar(255) not null,
+  providerUserId varchar(255),
+  rank int not null,
+  displayName varchar(255),
+  profileUrl varchar(512),
+  imageUrl varchar(512),
+  accessToken varchar(1024) not null,
+  secret varchar(255),
+  refreshToken varchar(255),
+  expireTime bigint,
+  primary key (userId, providerId, providerUserId));
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
+
+create table UserProfile (
+  userId varchar(255) not null,
+  email varchar(255),
+  firstName varchar(255),
+  lastName varchar(255),
+  name  varchar(255),
+  username varchar(255),
+  primary key (userId));
+create unique index UserProfilePK on UserProfile(userId);
+
+create table Data (
+  userId varchar(255) not null,
+  data varchar(1024),
+  primary key (userId));
+create unique index DataPK on Data(userId);
+
+CREATE  TABLE users (
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  enabled TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (username));
+  
+CREATE TABLE user_roles (
+  user_role_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  UNIQUE KEY uni_username_role (role,username),
+  KEY fk_username_idx (username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
